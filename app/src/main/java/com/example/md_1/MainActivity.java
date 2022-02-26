27e1a543b0617170b6d3fe295c90569c0b1116eb
EmilsKuz <kemils1323@gmail.com>
2022-02-26 13:46:58 +0200

    TakeAPic
    Take a picture and save it to image view

--- app/src/main/java/com/example/md_1/MainActivity.java
+++ app/src/main/java/com/example/md_1/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.md_1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int CAMERA_ACTION_CODE=1;
+    ImageView imageView1;
+    Button btn_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView1 = findViewById(R.id.imageView1);
+        btn_1 = findViewById(R.id.btn_1);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent, CAMERA_ACTION_CODE);
+                }else{
+                    Toast.makeText(MainActivity.this, "There is no app that support this action", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == CAMERA_ACTION_CODE && resultCode == RESULT_OK && data != null){
+            Bundle bundle = data.getExtras();
+            Bitmap finalPhoto = (Bitmap) bundle.get("data");
+            imageView1.setImageBitmap(finalPhoto);
+        }
+
     }
 }
